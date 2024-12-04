073c430476dbdef9e6c7461df7bdfbdacbb09298
Baluun wylon <matiw4444@gmail.com>
2024-12-04 15:50:08 +0000

    updated frontent

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -4,28 +4,30 @@ use crate::blog::Blog;
 
 mod blog;
 
-
-thread_local!
-{
+thread_local! {
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
-
+// komentarze ?
 
 #[ic_cdk::update]
-fn add_blog(title: String, date: u32, content: String, tags: Vec<String>) -> Vec<Blog> 
-{
-    let blog = Blog::new(title, date, content, tags);
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
+    if title.len() > 250 {
+        return Err("Title is too long!".to_string())
+    }
+    let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    let last_blog = BLOGS.with(|blogs| 
+        blogs
+        .borrow()
+        .last()
+        .expect("Vec should not be empty").clone());
+    Ok(last_blog)
 }
-
 
 #[ic_cdk::query]
-fn get_blogs() -> Vec<String> 
-{
+fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
-
-
 
 #[ic_cdk::query]
 fn greet(name: String) -> String {
